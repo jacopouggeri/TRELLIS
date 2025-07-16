04bc9b642939e6c21c57615cea936e9bce76790c
jacopouggeri <jacopo.uggeri@gmail.com>
2025-07-16 15:59:19 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import os
-# os.environ['ATTN_BACKEND'] = 'xformers'   # Can be 'flash-attn' or 'xformers', default is 'flash-attn'
+os.environ['ATTN_BACKEND'] = 'xformers'   # Can be 'flash-attn' or 'xformers', default is 'flash-attn'
 os.environ['SPCONV_ALGO'] = 'native'        # Can be 'native' or 'auto', default is 'auto'.
                                             # 'auto' is faster but will do benchmarking at the beginning.
                                             # Recommended to set to 'native' if run only once.
@@ -18,7 +18,6 @@ image = Image.open("assets/example_image/T.png")
 
 # Run the pipeline
 outputs = pipeline.run_from_noise(
-    image,
     seed=1,
     # Optional parameters
     # sparse_structure_sampler_params={
